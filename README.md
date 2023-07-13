a5066687a3268d074d1a02e182f72b3e76841af5
Anmol Singh <asingh@quora.com>
2023-07-12 21:53:13 -0700

    rotate key and fix casing

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# YT Summarizer Poe
+# YT summarizer bot
 
 Summarizes youtube videos. Available for use at: https://poe.com/VideoSummaries
--- main.py
+++ main.py
@@ -3,22 +3,23 @@
 # a message back at its user and is a good starting point for your bot, but you can
 # comment/uncomment any of the following code to try out other example bots.
 
-from modal import Image, Stub, asgi_app
+import os
+
+from modal import Image, Secret, Stub, asgi_app
 
 from yt_summarizer_bot import YTSummarizerBot
 
 # specific to hosting with modal.com
 image = Image.debian_slim().pip_install_from_requirements("requirements.txt")
-stub = Stub("yt-summarizer-bot")
+stub = Stub("yt-summarizer-app")
 
 
-@stub.function(image=image)
+@stub.function(image=image, secret=Secret.from_name("yt-summarizer-secret"))
 @asgi_app()
 def fastapi_app():
     from fastapi_poe import make_app
 
     bot = YTSummarizerBot()
-
-    POE_API_KEY = "j87ihqtkviux33dVDatxOrroHcvD1od3"
+    POE_API_KEY = os.environ["POE_API_KEY"]
     app = make_app(bot, api_key=POE_API_KEY)
     return app
